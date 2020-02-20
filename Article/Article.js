27553078149e9f18bf3f7650bcbae4280d395db1
Jose Sustaita <josesustaita70@gmail.com>
2020-02-20 18:51:35 -0500

    MVP met

--- Article/Article.js
+++ Article/Article.js
@@ -3,7 +3,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -23,8 +23,8 @@ const data = [
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -43,8 +43,8 @@ const data = [
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -71,8 +71,8 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -112,3 +112,106 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+const articles = document.querySelector(".articles");
+console.log(articles);
+
+function componentCreator(title, date, p1, p2, p3) {
+  //created elements
+  const article = document.createElement("div");
+  const titleContent = document.createElement("h2");
+  const titleDate = document.createElement("p");
+  const para1 = document.createElement("p");
+  const para2 = document.createElement("p");
+  const para3 = document.createElement("p");
+  const expandSpan = document.createElement("span");
+
+  //appended to the article div
+  article.appendChild(titleContent);
+  article.appendChild(titleDate);
+  article.appendChild(para1);
+  article.appendChild(para2);
+  article.appendChild(para3);
+  article.appendChild(expandSpan);
+
+  //added classes back in
+  article.classList.add("article");
+  titleDate.classList.add("date");
+  expandSpan.classList.add("expandButton");
+
+  //added the content to elements created
+  titleContent.textContent = title;
+  titleDate.textContent = date;
+  para1.textContent = p1;
+  para2.textContent = p2;
+  para3.textContent = p3;
+  expandSpan.textContent = "Click to Expand";
+
+  expandSpan.addEventListener("click", ({}) => {
+    article.classList.toggle("article-open");
+  });
+
+  return article;
+}
+
+data.map(article => {
+  return articles.appendChild(
+    componentCreator(
+      article.title,
+      article.date,
+      article.firstParagraph,
+      article.secondParagraph,
+      article.thirdParagraph
+    )
+  );
+});
+// Added new articles
+const addedArticles = [
+  {
+    title: "Jose's guide to software engineering",
+    date: "Feburary 18, 2020",
+    firstParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus ipsum vitae leo consectetur, sed varius diam rhoncus. Aliquam erat volutpat. Etiam pretium egestas lacus a laoreet. Vivamus vitae porttitor neque. Aliquam ornare interdum nulla ac congue. Proin auctor mauris eget ex eleifend consectetur fermentum sed ligula. Phasellus id orci sed dolor ultricies blandit luctus non turpis. Etiam id vestibulum felis, fringilla hendrerit turpis. Proin posuere nisl ut varius sodales. In ac odio non augue pretium vehicula non ac felis. Aliquam sapien nisi, mollis ac faucibus eget, pulvinar eget augue. Cras in sem in nunc aliquet cursus. Quisque vel tempus odio. Cras dapibus, tellus sed commodo suscipit, dui purus ultrices quam, in hendrerit nisl mauris eu risus.",
+
+    secondParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus ipsum vitae leo consectetur, sed varius diam rhoncus. Aliquam erat volutpat. Etiam pretium egestas lacus a laoreet. Vivamus vitae porttitor neque. Aliquam ornare interdum nulla ac congue. Proin auctor mauris eget ex eleifend consectetur fermentum sed ligula. Phasellus id orci sed dolor ultricies blandit luctus non turpis. Etiam id vestibulum felis, fringilla hendrerit turpis. Proin posuere nisl ut varius sodales. In ac odio non augue pretium vehicula non ac felis. Aliquam sapien nisi, mollis ac faucibus eget, pulvinar eget augue. Cras in sem in nunc aliquet cursus. Quisque vel tempus odio. Cras dapibus, tellus sed commodo suscipit, dui purus ultrices quam, in hendrerit nisl mauris eu risus.",
+
+    thirdParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus ipsum vitae leo consectetur, sed varius diam rhoncus. Aliquam erat volutpat. Etiam pretium egestas lacus a laoreet. Vivamus vitae porttitor neque. Aliquam ornare interdum nulla ac congue. Proin auctor mauris eget ex eleifend consectetur fermentum sed ligula. Phasellus id orci sed dolor ultricies blandit luctus non turpis. Etiam id vestibulum felis, fringilla hendrerit turpis. Proin posuere nisl ut varius sodales. In ac odio non augue pretium vehicula non ac felis. Aliquam sapien nisi, mollis ac faucibus eget, pulvinar eget augue. Cras in sem in nunc aliquet cursus. Quisque vel tempus odio. Cras dapibus, tellus sed commodo suscipit, dui purus ultrices quam, in hendrerit nisl mauris eu risus."
+  },
+  {
+    title: "Jose builds his own company",
+    date: "Feburary 19, 2020",
+    firstParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus ipsum vitae leo consectetur, sed varius diam rhoncus. Aliquam erat volutpat. Etiam pretium egestas lacus a laoreet. Vivamus vitae porttitor neque. Aliquam ornare interdum nulla ac congue. Proin auctor mauris eget ex eleifend consectetur fermentum sed ligula. Phasellus id orci sed dolor ultricies blandit luctus non turpis. Etiam id vestibulum felis, fringilla hendrerit turpis. Proin posuere nisl ut varius sodales. In ac odio non augue pretium vehicula non ac felis. Aliquam sapien nisi, mollis ac faucibus eget, pulvinar eget augue. Cras in sem in nunc aliquet cursus. Quisque vel tempus odio. Cras dapibus, tellus sed commodo suscipit, dui purus ultrices quam, in hendrerit nisl mauris eu risus.",
+
+    secondParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus ipsum vitae leo consectetur, sed varius diam rhoncus. Aliquam erat volutpat. Etiam pretium egestas lacus a laoreet. Vivamus vitae porttitor neque. Aliquam ornare interdum nulla ac congue. Proin auctor mauris eget ex eleifend consectetur fermentum sed ligula. Phasellus id orci sed dolor ultricies blandit luctus non turpis. Etiam id vestibulum felis, fringilla hendrerit turpis. Proin posuere nisl ut varius sodales. In ac odio non augue pretium vehicula non ac felis. Aliquam sapien nisi, mollis ac faucibus eget, pulvinar eget augue. Cras in sem in nunc aliquet cursus. Quisque vel tempus odio. Cras dapibus, tellus sed commodo suscipit, dui purus ultrices quam, in hendrerit nisl mauris eu risus.",
+
+    thirdParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus ipsum vitae leo consectetur, sed varius diam rhoncus. Aliquam erat volutpat. Etiam pretium egestas lacus a laoreet. Vivamus vitae porttitor neque. Aliquam ornare interdum nulla ac congue. Proin auctor mauris eget ex eleifend consectetur fermentum sed ligula. Phasellus id orci sed dolor ultricies blandit luctus non turpis. Etiam id vestibulum felis, fringilla hendrerit turpis. Proin posuere nisl ut varius sodales. In ac odio non augue pretium vehicula non ac felis. Aliquam sapien nisi, mollis ac faucibus eget, pulvinar eget augue. Cras in sem in nunc aliquet cursus. Quisque vel tempus odio. Cras dapibus, tellus sed commodo suscipit, dui purus ultrices quam, in hendrerit nisl mauris eu risus."
+  },
+  {
+    title: "Jose learns components",
+    date: "Feburary 20, 2020",
+    firstParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus ipsum vitae leo consectetur, sed varius diam rhoncus. Aliquam erat volutpat. Etiam pretium egestas lacus a laoreet. Vivamus vitae porttitor neque. Aliquam ornare interdum nulla ac congue. Proin auctor mauris eget ex eleifend consectetur fermentum sed ligula. Phasellus id orci sed dolor ultricies blandit luctus non turpis. Etiam id vestibulum felis, fringilla hendrerit turpis. Proin posuere nisl ut varius sodales. In ac odio non augue pretium vehicula non ac felis. Aliquam sapien nisi, mollis ac faucibus eget, pulvinar eget augue. Cras in sem in nunc aliquet cursus. Quisque vel tempus odio. Cras dapibus, tellus sed commodo suscipit, dui purus ultrices quam, in hendrerit nisl mauris eu risus.",
+
+    secondParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus ipsum vitae leo consectetur, sed varius diam rhoncus. Aliquam erat volutpat. Etiam pretium egestas lacus a laoreet. Vivamus vitae porttitor neque. Aliquam ornare interdum nulla ac congue. Proin auctor mauris eget ex eleifend consectetur fermentum sed ligula. Phasellus id orci sed dolor ultricies blandit luctus non turpis. Etiam id vestibulum felis, fringilla hendrerit turpis. Proin posuere nisl ut varius sodales. In ac odio non augue pretium vehicula non ac felis. Aliquam sapien nisi, mollis ac faucibus eget, pulvinar eget augue. Cras in sem in nunc aliquet cursus. Quisque vel tempus odio. Cras dapibus, tellus sed commodo suscipit, dui purus ultrices quam, in hendrerit nisl mauris eu risus.",
+
+    thirdParagraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus ipsum vitae leo consectetur, sed varius diam rhoncus. Aliquam erat volutpat. Etiam pretium egestas lacus a laoreet. Vivamus vitae porttitor neque. Aliquam ornare interdum nulla ac congue. Proin auctor mauris eget ex eleifend consectetur fermentum sed ligula. Phasellus id orci sed dolor ultricies blandit luctus non turpis. Etiam id vestibulum felis, fringilla hendrerit turpis. Proin posuere nisl ut varius sodales. In ac odio non augue pretium vehicula non ac felis. Aliquam sapien nisi, mollis ac faucibus eget, pulvinar eget augue. Cras in sem in nunc aliquet cursus. Quisque vel tempus odio. Cras dapibus, tellus sed commodo suscipit, dui purus ultrices quam, in hendrerit nisl mauris eu risus."
+  }
+];
+
+addedArticles.map(article => {
+  return articles.appendChild(
+    componentCreator(
+      article.title,
+      article.date,
+      article.firstParagraph,
+      article.secondParagraph,
+      article.thirdParagraph
+    )
+  );
+});
